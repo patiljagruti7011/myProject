9469eed328be079e47c22cc1567c8a960510e7be
patiljagruti7011 <patiljagruti7011@gmail.com>
2024-02-25 14:29:03 +0530

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <label> Count : {count}</label> <br />
+        <label> <h1> Count : {count}</h1></label> <br />
         <button onClick={() => setCount((count) => count + 1)}>
           Increase Count
         </button>
