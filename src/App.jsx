646726af461159bfe23880ddaa0f7f0393c77071
patiljagruti7011 <patiljagruti7011@gmail.com>
2024-02-25 13:40:16 +0530

    First GIT Commit with First React Project

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <label> Count : {count}</label> <br />
+        <button onClick={() => setCount((count) => count + 1)}>
+          Increase Count
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+          Decrease Count
+        </button>
+        <button onClick={() => setCount((count) => 0)}>
+          Reset Count
+        </button>
+        <br />
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
